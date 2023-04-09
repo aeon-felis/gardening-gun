e3839ed201d7b10739cb6e21a0e024de21848c64
Idan Arye <IdanArye@gmail.com>
2023-04-09 15:23:51 +0300

    Add a single tutorial level

--- .idanarye.moonicipal.lua
+++ .idanarye.moonicipal.lua
@@ -1,25 +1,18 @@
 local moonicipal = require'moonicipal'
 local T = moonicipal.tasks_file()
 
--- require'idan'.unload_package('idan.project')
+require'idan'.unload_package('idan.project')
 T = require'idan.project.rust.bevy'(T, {
     crate_name = 'gardening_gun',
-    cli_args_for_targets = {
-        ['gardening-gun'] = {
-            {'--level', 'test-level'},
-            {'--level', 'second-level'},
-        },
-    },
+    --cli_args_for_targets = {
+        --['gardening-gun'] = {
+            --{'--level', 'test-level'},
+            --{'--level', 'second-level'},
+        --},
+    --},
     extra_logging = {
         -- ['bevy_ecs::system::commands'] = 'info',
-    }
+    },
+    level_editor = true,
+    pkv_app_name = 'GardeningGun',
 })
-
-function T:go()
-    T:_simple_target_runner()('gardening-gun', '--editor')
-end
-
-function T:erase_save()
-    vim.cmd'!rm -Rf ~/.local/share/gardeninggun'
-end
-
